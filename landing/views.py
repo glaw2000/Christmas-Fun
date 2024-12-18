aa0e7fe5198bb1d0a09f806962a0a9c13aa25943
Grace Lawrence <grace.h.lawrence@gmail.com>
2024-12-18 12:48:06 +0000

    add coal.html and naughtyy_nice.html with url path and views in landing app

--- landing/views.py
+++ landing/views.py
@@ -8,3 +8,15 @@ class HomePage(TemplateView):
     Displays home page
     """
     template_name = 'landing/index.html'
+
+class Coal(TemplateView):
+    """
+    Displays Coal gift page
+    """
+    template_name = 'landing/coal.html'
+
+class NaughtyNice(TemplateView):
+    """
+    Displays Naughty or Nice Choice Page
+    """
+    template_name = 'landing/naughty_nice.html'
